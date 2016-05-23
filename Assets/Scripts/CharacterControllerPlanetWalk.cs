2df9a9a02c328247bdae18c84724cd5ed3b3044a
Benjamin Sergent <benjamin.sergent@daqri.com>
2016-05-22 23:41:40 -0700

    Added rigidbody controller that can walk around a sphere

---
+++ Assets/Scripts/CharacterControllerPlanetWalk.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterControllerPlanetWalk : MonoBehaviour {
+	public float speed = 6.0f;
+	public float gravityStrength = 9.8f;
+	public GameObject planetaryBody;
+
+	private CharacterController _charController;
+
+	void Start(){
+		_charController = GetComponent<CharacterController> ();
+	}
+
+	void Update () {
+		float deltaX = Input.GetAxis ("Horizontal") * speed;
+		float deltaZ = Input.GetAxis ("Vertical") * speed;
+		Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+		movement = Vector3.ClampMagnitude (movement, speed);
+		Vector3 toPlanet = (planetaryBody.transform.position - transform.position).normalized;
+		movement += (gravityStrength * toPlanet);
+		movement *= Time.deltaTime;
+		movement = transform.TransformDirection(movement);
+		_charController.Move (movement);
+	}
+}
---
+++ Assets/Scripts/RigidBodyPlanetWalker.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RigidBodyPlanetWalker : MonoBehaviour {
+	public float speed = 6.0f;
+	public float gravityStrength = 9.8f;
+	public GameObject planetaryBody;
+
+	private Rigidbody _body;
+
+	void Start(){
+		_body = GetComponent<Rigidbody> ();
+		_body.constraints = RigidbodyConstraints.FreezeRotationY;
+	}
+
+	void FixedUpdate () {
+		float deltaX = Input.GetAxis ("Horizontal") * speed;
+		float deltaZ = Input.GetAxis ("Vertical") * speed;
+		Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+		movement = Vector3.ClampMagnitude (movement, speed);
+		Vector3 toPlanet = (planetaryBody.transform.position - transform.position).normalized;
+
+		movement += (gravityStrength * toPlanet);
+
+		Debug.Log ("movement " + movement + " toward " + toPlanet);
+
+		_body.velocity = movement;
+		_body.transform.up = -toPlanet;
+	}
+}
