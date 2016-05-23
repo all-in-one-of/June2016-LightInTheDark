8962a2485b77b29caeb3a6624dd10af6e324d64d
Benjamin Sergent <benjamin.sergent@daqri.com>
2016-05-23 01:05:59 -0700

    Fixed several issues

--- Assets/Scripts/RigidBodyPlanetWalker.cs
+++ Assets/Scripts/RigidBodyPlanetWalker.cs
@@ -3,12 +3,14 @@ using System.Collections;
 
 public class RigidBodyPlanetWalker : MonoBehaviour {
 	public float speed = 100.0f;
+	public float angularSpeed = 100.0f;
 	public float gravityStrength = 9.8f;
 	public GameObject planetaryBody;
 
 	private Vector3 _relativeDown;
 	private Rigidbody _body;
 	private Animator _anim;
+	private Vector3 facing = Vector3.forward;
 
 	void Start(){
 		_body = GetComponent<Rigidbody> ();
@@ -17,31 +19,42 @@ public class RigidBodyPlanetWalker : MonoBehaviour {
 	}
 
 	void FixedUpdate () {
-		updateDown ();
+		UpdateDown ();	
+		UpdateVelocity ();
+		FixRotation ();
+	}
 
-		_body.velocity += getGravitationalAcceleration();
+	void UpdateVelocity() {
+		_body.velocity += GetGravitationalAcceleration();
 
-		_body.velocity = Vector3.Project (_body.velocity, _relativeDown);
-		Vector3 inputMovement = getInputMovement ();
+		Vector3 towardPlanet = Vector3.Project (_body.velocity, _relativeDown);
+		_body.velocity = towardPlanet;
+		Vector3 inputMovement = GetInputMovement ();
 		_body.velocity += inputMovement;
-		_body.transform.up = -_relativeDown;
 
 		_anim.SetFloat("Speed", inputMovement.magnitude);
 	}
 
-	void updateDown() {
+	void UpdateDown() {
 		_relativeDown = (planetaryBody.transform.position - transform.position).normalized;
 	}
 
-	Vector3 getInputMovement() {
-		float deltaX = Input.GetAxis ("Horizontal") * speed;
+	void FixRotation() {
+		float deltaX = Input.GetAxis ("Horizontal") * angularSpeed * Time.deltaTime;
+		facing = Quaternion.Euler (0, deltaX, 0) * facing;
+
+		Vector3 lookatPoint = Vector3.Cross(transform.right + facing, -_relativeDown);
+		transform.LookAt(transform.position + lookatPoint, -_relativeDown);
+	}
+
+	Vector3 GetInputMovement() {
 		float deltaZ = Input.GetAxis ("Vertical") * speed;
-		Vector3 inputMovement = new Vector3(deltaX, 0, deltaZ);
+		Vector3 inputMovement = new Vector3(0, 0, deltaZ);
 		inputMovement = Vector3.ClampMagnitude (inputMovement, speed);
 		return transform.TransformDirection(inputMovement);
 	}
 
-	Vector3 getGravitationalAcceleration() {
+	Vector3 GetGravitationalAcceleration() {
 		return gravityStrength * _relativeDown;
 	}
 }
