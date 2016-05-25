f8cfea6c096acf119376a43c8a0ad0f9675d6416
Benjamin Sergent <benjamin.sergent@daqri.com>
2016-05-25 00:08:27 -0700

    Launch improvements

--- Assets/Scripts/LaunchInDirection.cs
+++ Assets/Scripts/LaunchInDirection.cs
@@ -3,34 +3,48 @@ using System.Collections;
 
 public class LaunchInDirection : MonoBehaviour {
 	public float launchForce = 1000;
+	public float rotSpeed = 90;
 	public Vector3 launchDirection = new Vector3(0,1,1);
 
 	private bool _didLaunch = false;
 	private bool _doLaunch = false;
-	private LineRenderer shootVisualizer;
+	private LineRenderer _shotVisualizer;
 	private int _lineVertCount = 4;
-
 	private Rigidbody _body;
+
 	// Use this for initialization
 	void Start () {
 		_body = GetComponent<Rigidbody> ();
-		shootVisualizer = GetComponent<LineRenderer> ();
-		shootVisualizer.SetVertexCount (_lineVertCount);
-		shootVisualizer.SetWidth (0.5f, 0.1f);
-		shootVisualizer.SetColors (Color.blue, Color.blue);
+		_shotVisualizer = GetComponent<LineRenderer> ();
+		_shotVisualizer.SetVertexCount (_lineVertCount);
+		_shotVisualizer.SetWidth (0.5f, 0.1f);
+		_shotVisualizer.SetColors (Color.blue, Color.blue);
 
-		shootVisualizer.material.color = Color.blue;
+		_shotVisualizer.material.color = Color.blue;
+
+
+		_body.mass = float.PositiveInfinity;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		if (!_didLaunch && Input.GetButtonDown ("Launch")) {
+		if (_didLaunch) {
+			if (Input.GetButtonDown ("Launch")) {
+				_didLaunch = false; 
+				_shotVisualizer.SetVertexCount (_lineVertCount);
+			}
+			return;
+		}
+
+		if (Input.GetButtonDown ("Launch")) {
 			_doLaunch = true; 
 		}
 
-		if (!_didLaunch) {
-			DrawShootEstimate ();
-		}
+		float toRot = Input.GetAxis ("Horizontal") * rotSpeed * Time.deltaTime;
+		Quaternion newRotation = Quaternion.Euler (new Vector3 (0, toRot, 0));
+		launchDirection = newRotation * launchDirection;
+
+		DrawShootEstimate ();
 	}
 
 	void DrawShootEstimate () {
@@ -38,7 +52,7 @@ public class LaunchInDirection : MonoBehaviour {
 		float dist = launchForce / 100;
 
 		for (int index = 0; index < _lineVertCount; index++) {
-			shootVisualizer.SetPosition (index, transform.position + dist/_lineVertCount * index * launchDirection);	
+			_shotVisualizer.SetPosition (index, transform.position + dist/_lineVertCount * index * launchDirection);	
 		}
 	}
 
@@ -50,12 +64,13 @@ public class LaunchInDirection : MonoBehaviour {
 	}
 
 	void Launch() {
+		_body.mass = 1;
 		_doLaunch = false;
 		_didLaunch = true;
 		Debug.Log ("Launching");
 		var force = launchForce * launchDirection.normalized;
 		_body.AddForce (force);
-		shootVisualizer.SetVertexCount (0);
+		_shotVisualizer.SetVertexCount (0);
 	}
 
 	void MakeCameraFollowMe () {
---
+++ Assets/Scripts/Orbiter.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Orbiter : MonoBehaviour {
+	public float gravityAccel = 9.8f;
+
+	private Rigidbody _body;
+	private GameObject[] _planets;
+	// Use this for initialization
+	void Start () {
+		_planets = GameObject.FindGameObjectsWithTag ("Planet");
+		_body = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		for (int index = 0; index < _planets.Length; index++) {
+			_body.AddForce (getGravityVector (_planets[index]));
+		}
+	}
+
+	Vector3 getGravityVector(GameObject planet) {
+		Vector3 toPlanet = (planet.transform.position - transform.position);
+		float distSq = toPlanet.sqrMagnitude;
+
+		const float MAX_DIST_SQ = 1000 * 1000;
+		if (distSq > MAX_DIST_SQ) {
+			return Vector3.zero;
+		}
+
+		toPlanet /= Mathf.Sqrt (distSq);
+		return gravityAccel * toPlanet / distSq;
+	}
+		
+
+	float GetDistanceSqToPlanet(int index) {
+		return (transform.position - _planets [index].transform.position).sqrMagnitude;
+	}
+}
