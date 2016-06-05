874a174d1dba6759b138975783783978a1de1e72
Benjamin Sergent <benjamin.sergent@daqri.com>
2016-06-05 14:05:55 -0700

    Added the first prototype of grues. Ai not yet added.

---
+++ Assets/Scripts/GoForward.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoForward : MonoBehaviour {
+	public float speed = 1;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.Translate (Vector3.forward * (speed * Time.deltaTime));
+	}
+}
