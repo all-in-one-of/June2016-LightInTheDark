bff7eb9580fc38eaee398f039d1f9662beb935ad
Benjamin Sergent <benjamin.sergent@daqri.com>
2016-06-06 16:55:58 -0700

    Added to town test

---
+++ Assets/Scripts/Jumper.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jumper : MonoBehaviour {
+
+	public float minJumpTime = 1;
+	public float maxJumpTime = 3;
+	public float minJumpForce = 1000;
+	public float maxJumpForce = 10000;
+	private Rigidbody _body;
+	private float _tillJump;
+
+	// Use this for initialization
+	void Start () {
+		_body = GetComponent<Rigidbody> ();
+		_tillJump = Random.value;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		_tillJump -= Time.deltaTime;
+
+		if (_tillJump <= 0) {
+			Jump ();
+			DecideNextJumpTime ();
+		}
+	}
+
+	void Jump() {
+		float force = Random.value * (maxJumpForce - minJumpForce) + minJumpForce;
+		_body.AddForce (force * Vector3.up);
+	}
+		
+	void DecideNextJumpTime() {
+		_tillJump = minJumpTime + Random.value * (maxJumpTime - minJumpTime);
+	}
+}
---
+++ Assets/Scripts/Watcher.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Watcher : MonoBehaviour {
+	public Transform target;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.LookAt (target);
+	}
+}
