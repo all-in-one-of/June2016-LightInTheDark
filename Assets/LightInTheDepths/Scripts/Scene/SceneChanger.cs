b628a10995d13335fc4feb46c0f4f8cf667f81ae
Benjamin Sergent <benjamin.sergent@daqri.com>
2016-06-18 15:19:28 -0700

    Added first two scenes in actual game

---
+++ Assets/LightInTheDepths/Scripts/Scene/SceneChanger.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneChanger : MonoBehaviour {
+	public string sceneToLoad;
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Player") {
+			UnityEngine.SceneManagement.SceneManager.LoadScene (sceneToLoad);
+		}
+	}
+}
