7d5cc0fef22e241d292e272d5c26bfe2959ef2cd
Benjamin Sergent <benjamin.sergent@daqri.com>
2016-06-19 17:19:04 -0700

    Fixed inital water mushroom

--- Assets/LightInTheDepths/Scripts/Player/PlayerDeathHandler.cs
+++ Assets/LightInTheDepths/Scripts/Player/PlayerDeathHandler.cs
@@ -5,6 +5,7 @@ namespace LightInTheDark {
 [RequireComponent(typeof(PlayerState))]
 [RequireComponent(typeof(Animator))]
 public class PlayerDeathHandler : MonoBehaviour {
+	public bool deathEnabled = true;
 	private PlayerState _state;	
 
 	void Start() {
@@ -12,9 +13,11 @@ public class PlayerDeathHandler : MonoBehaviour {
 	}
 
 	void OnKilledByGrue() {
-		_state.isDieing = true;
-		Application.Quit(); // TODO: Handle properly
-		UnityEditor.EditorApplication.isPlaying = false;
+		if (deathEnabled) {
+			_state.isDieing = true;
+			Application.Quit (); // TODO: Handle properly
+			UnityEditor.EditorApplication.isPlaying = false;
+		}
 	}
 }
 }
