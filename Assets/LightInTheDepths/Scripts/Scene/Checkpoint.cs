e31b0ca7b066f5d1a07b1e9c6e909885e1cec3b9
Benjamin Sergent <benjamin.sergent@daqri.com>
2016-06-23 22:50:25 -0700

    Added checkpoint to end area

---
+++ Assets/LightInTheDepths/Scripts/Scene/Checkpoint.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Checkpoint : MonoBehaviour {
+	public float distanceToGet = 10;
+	private bool _playerHasBeenNear = false;
+	private GameObject _player;
+	// Use this for initialization
+	void Start () {
+		Object.DontDestroyOnLoad (this);
+		_player = GameObject.FindGameObjectWithTag ("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Vector3.Distance (_player.transform.position, transform.position) < distanceToGet) {
+			_playerHasBeenNear = true;
+		}
+	}
+
+	void OnLevelWasLoaded(int level) {
+		_player = GameObject.FindGameObjectWithTag ("Player");
+		if (_playerHasBeenNear) {
+			_player.transform.position = transform.position;
+		}
+	}
+}
