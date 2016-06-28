b4a89eabc841685e77141aa408d24fe48404ddec
Benjamin Sergent <benjamin.sergent@daqri.com>
2016-06-27 22:22:14 -0700

    Added main menu

---
+++ Assets/LightInTheDepths/Scripts/Scene/StartToContinue.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class StartToContinue : MonoBehaviour {
+	public UnityEngine.UI.Text text;	
+	private AsyncOperation _loadOp;
+	// Use this for initialization
+	void Start () {
+		_loadOp = SceneManager.LoadSceneAsync (0);
+		_loadOp.allowSceneActivation = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Accept")) {
+			Debug.Log ("Enter pressed");
+			text.text = "Loading...";
+			text.SetAllDirty ();
+			_loadOp.allowSceneActivation = true;			
+		}
+	}
+}
